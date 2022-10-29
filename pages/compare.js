0e6f543f740e0e487680cc3b090f5ad1f8767bef
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2022-10-29 21:06:56 +0200

    compare edit

--- pages/compare.js
+++ pages/compare.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 import React, { useState } from 'react'
 import { useEffect } from 'react';
 import ProductBox from '../components/ProductBox';
@@ -15,32 +14,21 @@ import ProductBox from '../components/ProductBox';
 //    }
 // }
 
-const compare = () => {
+const Compare = () => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        const auth = Cookies.get("auth")
-        if (!auth) {
-            const storedCompare = JSON.parse(localStorage.getItem("stored-compare")) || []
-            if (storedCompare.length < 1) {
-                storedCompare.push("---")
-            }
-            console.log(storedCompare)
-            axios.get('https://backends.donnachoice.com/api/products/?slug__in=' + storedCompare).then(res => {
-                setProducts(res.data)
-                setLoading(false)
-            })
-        } else {
-            axios.get('https://backends.donnachoice.com/api/products/compare/', {
-                headers: {
-                Authorization: `Bearer ${Cookies.get("token")}`,
-                },
-            }).then(res => {
-                setProducts(res.data)
-                setLoading(false)
-            })
+
+        const storedCompare = JSON.parse(localStorage.getItem("stored-compare")) || []
+        if (storedCompare.length < 1) {
+            storedCompare.push("---")
         }
+        console.log(storedCompare)
+        axios.get('https://backends.donnachoice.com/api/products/?slug__in=' + storedCompare).then(res => {
+            setProducts(res.data)
+            setLoading(false)
+        })
     }, []);
 
     if (loading) {
@@ -55,13 +43,13 @@ const compare = () => {
         <div className='container p-5 flex flex-col items-center sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
             {products.length == 0 ? "there is no products in compare yet."
                 : (
-                products.map(product => {
-                    return (
-                        <ProductBox key={product.id} product={product} />
-                    )
-                })
-            )}</div>
+                    products.map(product => {
+                        return (
+                            <ProductBox key={product.id} product={product} />
+                        )
+                    })
+                )}</div>
     )
 }
 
-export default compare
+export default Compare
