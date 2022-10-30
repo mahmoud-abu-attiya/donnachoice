252b4cec032f99b4571a59e373ff7fce33382ad0
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2022-10-30 15:29:20 +0200

    product err in deploy

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -33,7 +33,7 @@ const Product = ({ product }) => {
             <div className="flex items-center">
               <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
               <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
-                {product.name}
+                {product && product.name}
               </span>
             </div>
           </li>
@@ -45,7 +45,7 @@ const Product = ({ product }) => {
           <img src={product.img ? product.img : "https://www.peacemakersnetwork.org/wp-content/uploads/2019/09/placeholder.jpg"} alt="" />
         </div>
         <div className="flex flex-col gap-4">
-          <h2 className='text-2xl'>{product.name}</h2>
+          <h2 className='text-2xl'>{product && product.name}</h2>
           <p className='text-gray-600'>{product.description ? product.description : "no descrioption"}</p>
           <span className='text-xl text-gray-700'>$ {product.options[0].price}</span>
           <div className='flex gap-4'>
