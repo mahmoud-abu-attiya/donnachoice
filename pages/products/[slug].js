c4b5f8e5335f9c4eb8de73c00c6c307cb6ed7479
Ahmed Saber <ahmed0saber33@gmail.com>
2022-10-30 20:21:09 +0200

    handled wishlist, cart and compare in product page

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,13 +1,208 @@
-import React, { useEffect } from 'react'
-import Axios from 'axios';
-import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react'
 import ProductBox from '../../components/ProductBox';
-import { useState } from 'react';
+import { useDispatch } from 'react-redux'
+import { setAmount } from "../../slices/wishlistIndicatorSlice"
+import { setCartCount } from "../../slices/cartIndicatorSlice"
+import { setCompareCount } from "../../slices/compareIndicatorSlice"
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const getNumberOfProductsInWishlist = () => {
+  const storedWishlist = JSON.parse(localStorage.getItem("stored-wishlist")) || []
+  return storedWishlist.length
+}
+
+const getNumberOfProductsInCart = () => {
+  const storedCart = JSON.parse(localStorage.getItem("stored-cart")) || []
+  return storedCart.length
+}
+
+const getNumberOfProductsInCompare = () => {
+  const storedCompare = JSON.parse(localStorage.getItem("stored-compare")) || []
+  return storedCompare.length
+}
+
+const handleWishlistLocalStorage = (heartElement, itemSlug, changed) => {
+  const storedWishlist = JSON.parse(localStorage.getItem("stored-wishlist")) || []
+  if (storedWishlist.includes(itemSlug)) {
+    if (changed) {
+      storedWishlist.splice(storedWishlist.indexOf(itemSlug), 1)
+      heartElement.current.classList.remove("fas")
+      heartElement.current.classList.add("far")
+    } else {
+      heartElement.current.classList.add("fas")
+      heartElement.current.classList.remove("far")
+    }
+  } else {
+    if (changed) {
+      storedWishlist.push(itemSlug)
+      heartElement.current.classList.remove("far")
+      heartElement.current.classList.add("fas")
+    } else {
+      heartElement.current.classList.add("far")
+      heartElement.current.classList.remove("fas")
+    }
+  }
+  localStorage.setItem("stored-wishlist", JSON.stringify(storedWishlist))
+}
+
+const handleCartLocalStorage = (addToCartButton, itemId, changed) => {
+  const storedCart = JSON.parse(localStorage.getItem("stored-cart")) || []
+  const storedCartIds = storedCart.map(cartId => cartId.id)
+  if (storedCartIds.includes(itemId)) {
+    if (changed) {
+      console.log(storedCart)
+      console.log(itemId)
+      for (let i = 0; i < storedCart.length; i++) {
+          console.log("LOOP")
+          if (storedCart[i].id === itemId) {
+            console.log("IF", storedCart[i].id, itemId, i)
+            storedCart.splice(i, 1)
+            break
+          }
+      }
+      addToCartButton.textContent = "add"
+    } else {
+      addToCartButton.textContent = "remove"
+    }
+  } else {
+    if (changed) {
+      storedCart.push({
+          id: itemId,
+          amount: 1
+      })
+      addToCartButton.textContent = "remove"
+    } else {
+      addToCartButton.textContent = "add"
+    }
+  }
+  localStorage.setItem("stored-cart", JSON.stringify(storedCart))
+}
+
+const handleCompareLocalStorage = (compareElement, itemSlug, changed) => {
+  const storedCompare = JSON.parse(localStorage.getItem("stored-compare")) || []
+  if (storedCompare.includes(itemSlug)) {
+    if (changed) {
+      storedCompare.splice(storedCompare.indexOf(itemSlug), 1)
+      compareElement.current.classList.remove("fa-check-circle")
+      compareElement.current.classList.add("fa-balance-scale")
+    } else {
+      compareElement.current.classList.add("fa-check-circle")
+      compareElement.current.classList.remove("fa-balance-scale")
+    }
+  } else {
+    if (changed) {
+      storedCompare.push(itemSlug)
+      compareElement.current.classList.remove("fa-balance-scale")
+      compareElement.current.classList.add("fa-check-circle")
+    } else {
+      compareElement.current.classList.add("fa-balance-scale")
+      compareElement.current.classList.remove("fa-check-circle")
+    }
+  }
+  localStorage.setItem("stored-compare", JSON.stringify(storedCompare))
+}
 
 const Product = ({ product }) => {
-  const [relatedPro, setRelatedPro] = useState([]);
+  let storedCart, storedCartIds = []
+  const auth = Cookies.get("auth")
+  const heartIcon = useRef()
+  const compareIcon = useRef()
+  const optionsMenu = useRef()
+  const dispatch = useDispatch()
   useEffect(() => {
-    Axios.get(`https://backends.donnachoice.com/api/products/?parents__slug=${product.slug}`)
+      storedCart = JSON.parse(localStorage.getItem("stored-cart")) || []
+      storedCartIds = storedCart.map(cartId => cartId.id)
+      if (!auth) {
+        handleWishlistLocalStorage(heartIcon, product.slug, false)
+        handleCompareLocalStorage(compareIcon, product.slug, false)
+      }
+  }, [])
+  const [relatedPro, setRelatedPro] = useState([]);
+
+  const handleWishList = (item, isWish) => {
+    if (!auth) {
+      handleWishlistLocalStorage(heartIcon, item, true)
+      dispatch(setAmount(getNumberOfProductsInWishlist()))
+      return
+    }
+    isWish = heartIcon.current.classList.contains("fas")
+    console.log(isWish);
+    if (isWish) {
+      axios.post(`https://backends.donnachoice.com/api/products/remove_from_wishlist/`, {
+        products: [
+            item
+        ]
+      }, {
+        headers: {
+            Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      })
+      .then((res) => {
+          console.log(res.data)
+          heartIcon.current.classList.remove("fas")
+          heartIcon.current.classList.add("far")
+          axios.get(`https://backends.donnachoice.com/api/counts`, {
+            headers: {
+                Authorization: `Bearer ${Cookies.get("token")}`,
+            },
+          })
+            .then(res => {
+                dispatch(setAmount(res.data.wishlist))
+            })
+      })
+    } else {
+      axios.post(`https://backends.donnachoice.com/api/products/update_wishlist/`, {
+        products: [
+            item
+        ]
+      }, {
+        headers: {
+            Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data)
+        heartIcon.current.classList.add("fas")
+        heartIcon.current.classList.remove("far")
+        axios.get(`https://backends.donnachoice.com/api/counts`, {
+          headers: {
+              Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        })
+            .then(res => {
+              dispatch(setAmount(res.data.wishlist))
+            })
+      })
+    }
+  }
+
+  const handleCart = (cartBtn, itemId) => {
+    const auth = Cookies.get("auth")
+    if (!auth) {
+      handleCartLocalStorage(cartBtn, itemId, true)
+      dispatch(setCartCount(getNumberOfProductsInCart()))
+      return
+    }
+  }
+
+  const toggleOptionsMenu = () => {
+    if (optionsMenu.current) {
+      if (optionsMenu.current.classList.contains("hidden")) {
+          optionsMenu.current.classList.remove("hidden")
+      } else {
+          optionsMenu.current.classList.add("hidden")
+      }
+    }
+  }
+
+  const handleCompare = (item) => {
+    handleCompareLocalStorage(compareIcon, item, true)
+    dispatch(setCompareCount(getNumberOfProductsInCompare()))
+  }
+
+  useEffect(() => {
+    axios.get(`https://backends.donnachoice.com/api/products/?parents__slug=${product.slug}`)
       .then(res => setRelatedPro(res.data))
     console.log(product);
   }, [product])
@@ -49,18 +244,37 @@ const Product = ({ product }) => {
           <p className='text-gray-600'>{product.description ? product.description : "no descrioption"}</p>
           <span className='text-xl text-gray-700'>$ {product.options[0].price}</span>
           <div className='flex gap-4'>
-            <button
+          <div className='relative'>
+               <button
+                  className="text-white bg-primary-200 hover:bg-primary-300 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  onClick={() => toggleOptionsMenu(product.slug)}
+               >
+                  Add to cart
+               </button>
+               {product.options.length > 0 ? <div ref={optionsMenu} className='absolute right-0 top-full w-48 p-3 bg-white shadow rounded z-10 hidden'>
+                  {product.options.map(option => {
+                     return (<div key={option.id} className='grid grid-cols-3 option'>
+                        <span>{option.name}</span>
+                        <span>{option.price}$</span>
+                        <button data-slug={product.slug} onClick={(e) => handleCart(e.target, option.id)}>
+                           {storedCartIds.includes(option.id) ? "remove" : "add"}
+                        </button>
+                     </div>)
+                  })}
+               </div> : null}
+            </div>
+            {/* <button
               className="text-white w-full bg-primary-200 hover:bg-primary-300 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            // onClick={() => dispatch(increment(props.product.slug))}
+            // onClick={() => dispatch(increment(product.slug))}
             >
               Add to cart
+            </button> */}
+            <button className='z-10 text-xl border rounded px-4 bg-gray-100 hover:shadow transition hover:scale-105 text-red-600' title='Add product to wishlist' onClick={() => handleWishList(product.slug, product.is_wishlist)}>
+              {/* <button className='z-10' onClick={() => handleWishList(product.slug, product.is_wishlist)}> */}
+              <i ref={heartIcon} className="far fa-heart"></i>
             </button>
-            <button className='z-10 text-xl border rounded px-4 bg-gray-100 hover:shadow transition hover:scale-105 text-red-600' title='Add product to wishlist' onClick={() => handleWishList(props.product.slug, props.product.is_wishlist)}>
-              {/* <button className='z-10' onClick={() => handleWishList(props.product.slug, props.product.is_wishlist)}> */}
-              <i className="far fa-heart"></i>
-            </button>
-            <button className='z-10 text-xl border rounded px-4 bg-gray-100 hover:shadow transition hover:scale-105 text-blue-600' title='Add product to comper list' >
-              <i className="fas fa-balance-scale"></i>
+            <button className='z-10 text-xl border rounded px-4 bg-gray-100 hover:shadow transition hover:scale-105 text-blue-600' title='Add product to comper list' onClick={() => handleCompare(product.slug)}>
+              <i ref={compareIcon} className="fas fa-balance-scale"></i>
             </button>
           </div>
         </div>
@@ -79,7 +293,7 @@ const Product = ({ product }) => {
 export default Product;
 
 export const getStaticProps = async ({ params }) => {
-  const { data } = await Axios.get(`https://backends.donnachoice.com/api/products/${params.slug}`);
+  const { data } = await axios.get(`https://backends.donnachoice.com/api/products/${params.slug}`);
   const product = data;
   return {
     props: {
@@ -89,7 +303,7 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export const getStaticPaths = async () => {
-  const { data } = await Axios.get("https://backends.donnachoice.com/api/products/");
+  const { data } = await axios.get("https://backends.donnachoice.com/api/products/");
   const paths = data.map((products) => ({ params: { slug: products.slug.toString() } }));
   return {
     paths,
