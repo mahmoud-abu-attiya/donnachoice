82d16a9d8d0610dead4aa1ff1518a34e2fd1102c
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2022-11-08 18:49:57 +0200

    swiper images in home hero

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -8,12 +8,11 @@ import "swiper/css/autoplay"
 import { Autoplay, Pagination, Navigation } from "swiper";
 import axios from "axios";
 
-
 export default function App() {
    const [swiperImgs, setSwiperImgs] = useState([])
+
    useEffect(() => {
-      axios.get("https://backends.donnachoice.com/api/swiper/").then(res => console.log(res.data))
-   // console.log(swiperImgs);
+      axios.get("https://backends.donnachoice.com/api/swiper/").then(res => setSwiperImgs(res.data))
    }, []);
    return (
       <div className="homeslider">
@@ -31,33 +30,13 @@ export default function App() {
             modules={[Autoplay, Pagination, Navigation]}
             className="mySwiper homehero"
          >
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src="https://donnachoice.com/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-25-at-3.34.08-PM.jpeg" className="min-h-full min-w-full object-cover" alt="" />
-            </SwiperSlide>
+            {swiperImgs.length > 0 && swiperImgs.map(swiper => {
+               return (
+                  <SwiperSlide key={swiper.id} className="flex justify-center items-center">
+                     <img src={swiper.img} className="min-h-full min-w-full object-cover" alt="" />
+                  </SwiperSlide>
+               )
+            })}
          </Swiper>
       </div>
    );
