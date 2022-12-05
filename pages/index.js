b741be2b153564e7a773fce823378f849840b838
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2022-12-05 20:24:30 +0200

    remove getStaticProps from home

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
+import axios from "axios";
 import Hero from "../components/HomeHero"
 import Section from "../components/Sections"
+import { useState, useEffect } from "react";
+import HomeP from "../components/placeholder/HomeP";
 
-export const getStaticProps = async () => {
-  const res = await fetch('https://backends.donnachoice.com/api/category/');
-  let sections = await res.json();
-  return {
-    props: {
-      sections: sections
-    }
+export default function Home() {
+  const [sections , setSections] = useState([])
+  const [loading , setLoading ] = useState(true)
+  useEffect(() => {
+    axios.get("https://backends.donnachoice.com/api/category/")
+    .then(res => {
+      // console.log(res.data);
+      setSections(res.data)
+      setLoading(false);
+    })
+  }, [])
+  if (loading) {
+    return <HomeP />
   }
-}
-
-export default function Home({ sections }) {
   return (
     <div>
       <Hero />
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-10 py-8">
-          {sections.map((section) => {
+          {sections?.map((section) => {
             return (
               <Section
                 key={section.id}
